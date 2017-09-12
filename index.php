4317a71b8f10692b925c3f1ca1c4470070cb2bee
ozols <ozols@10.175.164.140>
2017-09-12 09:56:11 +0200

    added menu

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        item1
+                    </li>
+                    <li>
+                        item2
+                    </li>
+                    <li>
+                        item3
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
