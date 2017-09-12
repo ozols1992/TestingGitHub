bc6296d23455d1a3d6c4cea1ce1ab30aff32b1c8
Marianne Nielsen <haffemn@gmail.com>
2017-09-12 08:58:27 +0200

    MN made a change

--- xampp/htdocs/TestingGitHub/index.php
+++ xampp/htdocs/TestingGitHub/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+            echo 'Marianne made a change here';
         ?>
     </body>
 </html>
